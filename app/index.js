6b43691f9e31dba0bd79d4b5538767c4fccdd8fc
Rokas <rookas.rudzenskas@gmail.com>
2023-06-02 20:35:59 +0300

    🎨 style(_layout.js): remove unused code and file
    🚀 chore(index.js): add a question to the page
    
    🚀 chore(QuestionListItem.tsx): add question data to the component and style it

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import QuestionListItem from "../src/components/QuestionListItem";
+import questions from '../data/questions.json'
 
 export default function Page() {
+  const question = questions.items[0]
   return (
     <View className="flex flex-1 bg-white">
-      <QuestionListItem />
+      <View className="mx-5">
+        <QuestionListItem />
+      </View>
     </View>
   );
 }
--- src/components/QuestionListItem/QuestionListItem.tsx
+++ src/components/QuestionListItem/QuestionListItem.tsx
@@ -1,15 +1,49 @@
-//@ts-nocheck
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+// @ts-nocheck
+import { View, Text, StyleSheet } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import questions from '../../../data/questions'
 
-const QuestionListItem = () => {
+const QuestionListItem = ({ }) => {
+  const question = questions.items[0];
   return (
-    <View>
-      <Text>
-        byrookas 🚀
+    <View style={styles.container}>
+      <Text className="flex items-center text-xs">
+        {question.score} votes •{' '}
+        {question.is_answered && (
+          <Entypo name="check" size={12} color="limegreen" />
+        )}
+        {question.answer_count} answers • {question.view_count} views
       </Text>
+      <Text className="text-[16px] text-[#0063bf] my-2">{question.title}</Text>
+      <Text style={{lineHeight: 15}} className="text-xs text-gray-500" numberOfLines={2}>
+        {question.body_markdown}
+      </Text>
+      <View className="flex flex-row my-3 items-center" style={{flexWrap: 'wrap', gap: 5,}}>
+        {question.tags.map((tag) => (
+          <Text key={tag} style={styles.tag}>{tag}</Text>
+        ))}
+        <Text className="text-xs text-[#6a737c]" style={{marginLeft: 'auto',}}>
+          asked {new Date(question.creation_date * 1000).toDateString()}
+        </Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderBottomWidth: 0.5,
+    borderColor: 'lightgray',
+  },
+  tag: {
+    backgroundColor: '#e1ecf4',
+    color: '#39739d',
+    padding: 5,
+    fontSize: 12,
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+});
 
 export default QuestionListItem;
