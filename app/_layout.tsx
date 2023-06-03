9470dce70874ea02d360f25aefa1cd1ae4ebe591
Rokas <rookas.rudzenskas@gmail.com>
2023-06-03 09:19:27 +0300

    🎨 style(_layout.tsx): add search icon to headerRight of index screen to improve user experience

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
-import { Stack, } from 'expo-router';
+import {Link, Stack,} from 'expo-router';
 import {Provider} from "urql";
 import {client} from "../src/graphql/client";
+import {AntDesign} from "@expo/vector-icons";
 
 const RootLayout = () => {
   return (
     <Provider value={client}>
       <Stack>
-        <Stack.Screen name="index" options={{ title: 'StackOverflow' }} />
+        <Stack.Screen name="index" options={{
+          title: 'StackOverflow',
+          headerRight: () => (
+            <Link href={`/search`}>
+              <AntDesign name="search1" size={20} color="dimgray" />
+            </Link>
+          )
+        }} />
       </Stack>
     </Provider>
   );
