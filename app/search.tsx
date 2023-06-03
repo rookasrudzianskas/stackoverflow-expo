3d1846db89291b0ebdbf481cb4ac04e5a8c2f119
Rokas <rookas.rudzenskas@gmail.com>
2023-06-03 09:16:52 +0300

    ✨ feat(search.tsx): add Search component to render a simple text message in the app

---
+++ app/search.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Search = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default Search;
