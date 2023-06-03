b5617468ed6d5e5d7fd40d3f23a1f07e04b0abe5
Rokas <rookas.rudzenskas@gmail.com>
2023-06-03 08:50:44 +0300

    🔥 chore([id].tsx): remove unused import statement for questions.json ✨ feat([id].tsx): add urql library to fetch question data from GraphQL API 🐛 fix([id].tsx): handle loading and error states when fetching question data from GraphQL API

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,16 +1,34 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {Text, View, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 import {useSearchParams} from "expo-router";
-import questions from '../data/questions.json';
 import AnswerListItem from "../src/components/AnswerListItem";
 import QuestionHeader from "../src/components/QuestionHeader";
 import answers from '../data/answers.json';
+import {getQuestionQuery} from "../src/graphql/queries";
+import {useQuery} from "urql";
 
 const QuestionDetailsPage = () => {
   const {id} = useSearchParams();
-  const question = questions.items.find((q) => q.question_id == id);
-  if(!question) return (<Text>Question not found</Text>);
+  const [result] = useQuery({
+    query: getQuestionQuery,
+    variables: { id },
+  });
+
+  if (result.fetching) {
+    return (
+      <View className="h-screen flex items-center justify-center">
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  if (result.error) {
+    return (
+      <View className="h-screen flex items-center justify-center">
+        <Text>Error: {result.error.message}</Text>
+      </View>
+    );
+  }
 
   return (
     <View className="mx-4">
