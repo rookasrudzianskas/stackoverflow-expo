e0f0ba75c9fa65f4663a4c9217bcde2f39e4f574
Rokas <rookas.rudzenskas@gmail.com>
2023-06-03 09:24:03 +0300

    🚀 feat(search.tsx): add search bar to header with onBlur event to trigger search function and onChangeText event to update search term state. Use useLayoutEffect hook to set header options and navigation options.

--- app/search.tsx
+++ app/search.tsx
@@ -1,8 +1,26 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {useNavigation} from "expo-router";
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigation = useNavigation();
+
+  const search = () => {
+    console.log('searching')
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerSearchBarOptions: {
+        onChangeText: (event) => setSearchTerm(event.nativeEvent.text),
+        onBlur: search,
+      },
+    });
+  }, [navigation, searchTerm, setSearchTerm]);
+
   return (
     <View>
       <Text>
